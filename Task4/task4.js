dd4606f628e7adc3e885628e68c81e1836d15720
chjagadesh328 <chjagadesh328>
2023-11-18 21:33:45 +0530

    task 4 js file of 3rd element green bgcolor and bold

---
+++ Task4/task4.js
@@ -0,0 +1,10 @@
+
+//
+var main=document.getElementsByClassName('list-group-item');
+main[2].style.backgroundColor='green';
+for(let i=0;i<main.length;i++)
+{
+  main[i].style.fontWeight='bold';
+ 
+
+}
